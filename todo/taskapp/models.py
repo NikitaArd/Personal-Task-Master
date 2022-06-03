53ee72a11af4c8485a26d927fed9c1c287ced961
Nikita <11nik11255@gmail.com>
2022-06-03 19:11:26 +0200

    Create git ignore for cache files and create models but without migrations

--- todo/taskapp/models.py
+++ todo/taskapp/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+# Code from https://tech.serhatteker.com/post/2020-01/email-as-username-django/
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=35, blank=False, verbose_name='User name')
+    email = models.EmailField(unique=True, blank=False, verbose_name='User e-mail')
+
+    USERNAME_FIELD = email
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=300, blank=False, verbose_name='Task title')
+    doneStatus = models.BooleanField(default=False, verbose_name='Done ?')
+    date = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Time and Data')
+    byUser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='By')
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        ordering = ['date']
+
+
+
