89c6560ad3edbe1385f11a348ebafd5d347991f1
Nikita <11nik11255@gmail.com>
2022-06-06 22:28:33 +0200

    Create logout view

--- todo/taskapp/urls.py
+++ todo/taskapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+
 from .views import index
 from .views import CustomLoginView
 
 urlpatterns = [
     path('', index, name='index'),
     path('login/', CustomLoginView, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -102,6 +102,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'taskapp.CustomUser'
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
