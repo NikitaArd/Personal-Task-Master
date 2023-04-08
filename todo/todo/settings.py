cb91f401ee8ba9b78f2e5ed0750a795e8ef02ccf
NikitaArd <11nik11255@gmail.com>
2023-04-08 18:27:32 +0200

    Switched SQLite to PostgreSQL

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
